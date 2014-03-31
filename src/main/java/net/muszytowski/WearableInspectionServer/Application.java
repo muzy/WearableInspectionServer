1e1ea511e343a29ce850c98c51c02204e019e9e5
Armin Bauer <git@eulerpfad.com>
2014-03-31 22:08:58 +0200

    added application.properties to initialise the application

--- src/main/java/net/muszytowski/WearableInspectionServer/Application.java
+++ src/main/java/net/muszytowski/WearableInspectionServer/Application.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySources;
+import org.springframework.context.annotation.PropertySource;
 
 @ComponentScan
 @Configuration
 @EnableAutoConfiguration
+@PropertySources(value = {@PropertySource("classpath:application.properties")})
 public class Application {
 
     public static void main(String[] args) {
