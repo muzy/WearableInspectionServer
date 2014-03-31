ddff0a68f5e970955d1bd0a0905b0c81d9fac4e4
Sebastian Muszytowski <sebastian@muszytowski.net>
2014-03-31 16:33:45 +0200

    remove upload stuff

--- src/main/java/net/muszytowski/WearableInspectionServer/Application.java
+++ src/main/java/net/muszytowski/WearableInspectionServer/Application.java
@@ -1,11 +1,7 @@
 package net.muszytowski.WearableInspectionServer;
 
-import javax.servlet.MultipartConfigElement;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.context.embedded.MultiPartConfigFactory;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,12 +14,4 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
     
-    @Bean
-    MultipartConfigElement multipartConfigElement() {
-        MultiPartConfigFactory factory = new MultiPartConfigFactory();
-        factory.setMaxFileSize("128MB");
-        factory.setMaxRequestSize("128MB");
-        return factory.createMultipartConfig();
-    }
-
 }
